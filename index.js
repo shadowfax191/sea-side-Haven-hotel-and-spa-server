743088f2e3ad1099a33342749cfa501e8fe4961c
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2023-11-07 00:58:54 +0600

    bookingData post api

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json());
 
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.userId}:${process.env.password}@my-hotel.rge90y4.mongodb.net/?retryWrites=true&w=majority`;
 
