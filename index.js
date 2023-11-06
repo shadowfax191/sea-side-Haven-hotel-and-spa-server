92fbd68be5d01f86a87d3119a88e2eabe5a58ecc
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2023-11-06 23:55:35 +0600

    connecting mongodb

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://shahariarhossain0599:6yl89pNgpLdY50iL@my-hotel.rge90y4.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -72,7 +72,6 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
 
     }
 }
